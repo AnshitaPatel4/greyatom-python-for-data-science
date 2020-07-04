1e70d6e963f9eca55445e1d0bd4fdd162c07e5d4
AnshitaPatel4 <AnshitaPatel4@users.noreply.github.com>
2020-07-04 14:54:34 +0530

    Code file created.

---
+++ python-for-data-science/code.py
@@ -0,0 +1,77 @@
+# --------------
+# Code starts here
+class_1=['Groffrey Hinton', 'Andrew Ng', 'Sebastian Raschka', 'Yoshua Bengio']
+class_2=['Hilary Mason', 'Carla Gentry', 'Corinna Cortes']
+# Create the lists 
+new_class= class_1+class_2
+print(new_class);
+# Concatenate both the strings
+new_class.append('Peter Warden')
+print(new_class);
+# Append the list
+new_class.remove('Carla Gentry')
+print(new_class);
+# Print updated list
+courses={"Math": [65], "English": [70], "History": [80], "French": [70], "Science": [60]}
+print(courses);
+
+
+# Remove the element from the list
+
+# Print the list
+
+
+
+# Create the Dictionary
+
+
+
+# Slice the dict and stores  the all subjects marks in variable
+total=65+70+80+70+60
+print(total);
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+percentage=total/500
+
+# Insert percentage formula
+print(percentage);
+# Print the percentage
+mathematics={"Geoffrey Hinton": [78], "Andrew Ng": [95], "Sebastian Raschka": [65], "Yoshua Benjio": [50], "Hilary Mason": [70], "Corinna Cortes": [66], "Peter Warden": [75]}
+max_marks_scored = max (mathematics,key = mathematics.get)
+print(max_marks_scored);
+
+
+
+# Create the Dictionary
+topper="""Andrew   Ng"""
+print('-'*20)
+first_name=print(topper.split()[0])
+last_name=print(topper.split()[1])
+full_name='Ng'+' ' + 'Andrew'
+certificate_name=full_name.upper()
+print(certificate_name);
+
+
+
+
+
+
+
+# Given string
+
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
